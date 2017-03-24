08cfa7bb02865aea5aba8b49a8e9344996e72f5b
Tiago Inácio <tiago.inacio@blip.pt>
2017-03-24 19:56:18 +0000

    add tests to genotype

--- test/genotype.cc
+++ test/genotype.cc
@@ -16,12 +16,15 @@
 #include "test/to_vec.h"
 
 using testing::ElementsAre;
+using testing::Ge;
+using testing::Le;
+using testing::AllOf;
 
-TEST(genotype_constructor, function_genes) {
+TEST(genotype_constructor, function_genes_5_elements) {
     cgp::Configuration configuration;
     configuration.setConnections(0);
     configuration.setParameters(0);
-    configuration.setFunctions(2);
+    configuration.setFunctions(5);
 
     std::shared_ptr<cgp::Size> size(new cgp::Size(configuration));
     std::shared_ptr<cgp::GeneType> gene_type(new cgp::GeneType(size));
@@ -30,7 +33,21 @@ TEST(genotype_constructor, function_genes) {
 
     genotype.create(state, size, gene_type);
 
-    const int genes_[] = {1};
+    EXPECT_THAT(genotype.genes()[0], AllOf(Ge(0),Le(5)));
+}
 
-    EXPECT_EQ(genotype.genes(), toVec(genes_, genotype.genes().size()));
+TEST(genotype_constructor, function_genes_1_element) {
+    cgp::Configuration configuration;
+    configuration.setConnections(0);
+    configuration.setParameters(0);
+    configuration.setFunctions(1);
+
+    std::shared_ptr<cgp::Size> size(new cgp::Size(configuration));
+    std::shared_ptr<cgp::GeneType> gene_type(new cgp::GeneType(size));
+    std::shared_ptr<cgp::State> state(new cgp::State);
+    cgp::Genotype genotype;
+
+    genotype.create(state, size, gene_type);
+
+    EXPECT_THAT(genotype.genes()[0], 1);
 }
