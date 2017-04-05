07fdfc9ad8c7814e80af73a298aa956ea7826ff7
Tiago Inácio <tiago.inacio@blip.pt>
2017-04-05 17:21:18 +0100

    update symbolic regression example

--- examples/symbolic-regression.cc
+++ examples/symbolic-regression.cc
@@ -6,9 +6,9 @@
 
 #include "include/cgp.h"
 #include "include/configuration.h"
+#include "include/fitness_args.h"
 #include "include/parameter.h"
 #include "include/state.h"
-#include "include/fitness_args.h"
 
 int plus(int x) {
     return x + 2;
@@ -28,62 +28,93 @@ void callbackOnInit(const cgp::Event& event) {
 }
 
 double fitnessFunction(cgp::FitnessArgs<double> args) {
-    std::cout << "calling fitness function: " << args.state()->generation() << std::endl;
+    std::cout << "calling fitness function: " << args.state()->generation()
+              << std::endl;
     return 1;
 }
 
+class Fitness {
+ public:
+    static double fn(cgp::FitnessArgs<double> args) {
+        return 2;
+    }
+};
+
+class ProgramInputs {
+ public:
+    ProgramInputs() {
+    }
+};
+
+class Configuration {
+ public:
+    static std::shared_ptr<cgp::Configuration> create() {
+        std::shared_ptr<cgp::Configuration> configuration(new cgp::Configuration());
+
+        configuration->isNodeOutputTheLastOne(false);
+        configuration->setRows(1);
+        configuration->setColumns(10);
+        configuration->setLevelsBack(10);
+        configuration->setComparisonOperator("<=");
+        configuration->setFitnessThreshold(0.1);
+        configuration->setGenerations(1000);
+        configuration->setRuns(5);
+        configuration->setFunctions(4);
+        configuration->setMutationProbability(0.2);
+        configuration->setOffspring(4);
+        configuration->setProgramOutputs(1);
+        configuration->setProgramInputs(2);
+        configuration->setConnections(2);
+        configuration->setParameters(0);
+
+        return configuration;
+    }
+};
+
 class InstructionSet {
  public:
-    static double fn2(std::vector<double> args) {
+    static double sum(std::vector<double> args) {
         return args[0] + args[1];
+    }
+    static double subtract(std::vector<double> args) {
+        return args[0] - args[1];
+    }
+    static double times(std::vector<double> args) {
+        return args[0] * args[1];
+    }
+    static double division(std::vector<double> args) {
+        if (args[1] != 0) {
+            return args[0] / args[1];
+        }
+        return args[0];
     }
 };
 
 int main() {
     // Configuration
-    std::shared_ptr<cgp::Configuration> configuration(new cgp::Configuration());
-
-    configuration->isNodeOutputTheLastOne(false);
-    configuration->setColumns(10);
-    configuration->setComparisonOperator("<=");
-    configuration->setFitnessThreshold(0.1);
-    configuration->setGenerations(100);
-    configuration->setLevelsBack(2);
-    configuration->setFunctions(2);
-    configuration->setMutationProbability(0.1);
-    configuration->setOffspring(4);
-    configuration->setProgramOutputs(1);
-    configuration->setProgramInputs(2);
-    configuration->setConnections(2);
-    configuration->setParameters(2);
-    configuration->setRows(1);
-    configuration->setRuns(5);
-
-    cgp::CGP<double> cgp(configuration);
+    cgp::CGP<double> cgp(Configuration::create());
 
     // Parameters
-    std::shared_ptr<cgp::Parameter<int> > firstParam =
-        cgp::createParameter<int>(2, &plus);
-    std::shared_ptr<cgp::Parameter<std::string> > secondParam =
-        cgp::createParameter<std::string>();
+    // std::shared_ptr<cgp::Parameter<int> > firstParam =
+    //     cgp::createParameter<int>(2, &plus);
+    // std::shared_ptr<cgp::Parameter<std::string> > secondParam =
+    //     cgp::createParameter<std::string>();
+    //
+    // secondParam->setValue("old text");
+    // secondParam->setMutationFn(changeText);
 
-    secondParam->setValue("old text");
-    secondParam->setMutationFn(changeText);
-
-    cgp.pushParameter(firstParam);
-    cgp.pushParameter(secondParam);
-
-    std::cout << firstParam->getValue() << std::endl;
-    std::cout << secondParam->getValue() << std::endl;
+    // cgp.pushParameter(firstParam);
+    // cgp.pushParameter(secondParam);
 
     cgp.subscribe("on_init", callbackOnInit);
 
-    cgp.pushFunction(fn1);
-    cgp.pushFunction(&InstructionSet::fn2);
+    cgp.pushFunction(&InstructionSet::sum);
+    cgp.pushFunction(&InstructionSet::subtract);
+    cgp.pushFunction(&InstructionSet::times);
+    cgp.pushFunction(&InstructionSet::division);
 
-    cgp.addFitnessFunction(fitnessFunction);
+    cgp.addFitnessFunction(&Fitness::fn);
 
-    // Run
     cgp.run();
 
     return 0;
